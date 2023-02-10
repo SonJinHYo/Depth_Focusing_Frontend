b156f3b1e2821ead286aa8038ff5cc2afc2ded45
SonJinHYo <wlsgy95@naver.com>
2023-02-10 21:35:56 +0900

    tnwjd

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/api/v1/"
-      : "https://back.endhttp://depth-focusing.xyz/api/v1/",
+      : "https://backend.http://depth-focusing.xyz/api/v1/",
   withCredentials: true,
 });
 
